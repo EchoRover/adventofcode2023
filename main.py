e7a0ecd0386b769646ca1c2e89dbc87ec6cdc5f7
Evan Tobias <105860929+EchoRover@users.noreply.github.com>
2023-12-05 23:14:19 +0400

    added more main features

--- main.py
+++ main.py
@@ -5,16 +5,28 @@ import time
 defaults = """
 test = \"""  \"""
 
+
+def parser(data):
+    pass
+
 def main(data):
     pass
 
 
-with open("data.txt") as f:
-    dd = f.read()
+with open("data.txt") as file:
+    input = file.read()
     main(test)
 
 """
+defaults2 = """
 
+with open("part1.py") as file:
+    data = file.read()
+
+with open("part2.py","w") as file:
+    file.write(data)
+
+"""
 
 
 def setupday(day = None):
@@ -29,12 +41,27 @@ def setupday(day = None):
     f = open(dirname + "/part1.py","w")
     f.write(defaults.format(day = day))
     f.close()
-    f = open(dirname + "/part2.py","a").close()
+    f = open(dirname + "/part2.py","a")
+    f.write(defaults2.format(day = day))
+    f.close()
     f = open(dirname + "/data.txt","a").close()
+
+
+
+def sample():
+    pass
+    if __name__ == "__main__":
+        exit()
+    print(__file__)
+    return
+    with open(__file__,"w") as file:
+        file.write(defaults)
+
 
 
 if __name__ == "__main__":
     setupday()
+    
 
 
 
