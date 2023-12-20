791bbcbe76071b9e2b14bcf5ac01cfbbb67199c7
Evan Tobias <105860929+EchoRover@users.noreply.github.com>
2023-12-20 12:09:11 +0400

    day17 done

--- day17/part1.py
+++ day17/part1.py
@@ -1,4 +1,4 @@
-
+from heapq import heappush,heappop
 test = """2413432311323
 3215453535623
 3255245654254
@@ -14,101 +14,83 @@ test = """2413432311323
 4322674655533"""
 
 
+
+
+
+
+class Heatsolver:
+    def __init__(self,data):
+        self.data = data
+        self.length = len(self.data[0])
+        self.breadth = len(self.data)
+
+        self.solve()
+    
+    def solve(self):
+        # x,y drx,dry dist ,steps,heat
+        self.queue = [(0,0,0,0,0,0)]
+        self.seen = set()
+
+        while len(self.queue) != 0:
+            # self.queue.sort()
+            
+            heat,x,y,drx,dry,dist = heappop(self.queue)
+            
+            # self.display(heat)
+
+            # print(x,y,drx,dry,dist,heat)
+
+            if (x == (self.length - 1)) and (y == (self.breadth - 1)):
+                print("Heat is :")
+                print(heat)
+                return
+                
+
+            if (x,y,drx,dry,dist) in self.seen:
+                continue
+            self.seen.add((x,y,drx,dry,dist))
+
+   
+           
+
+            for dx,dy in [(0,-1),(1,0),(0,1),(-1,0)]:    
+                if (dx == -drx) and (dy == -dry):
+                    continue
+                
+                nx = x + dx
+                ny = y + dy
+                if nx in range(self.length) and ny in range(self.breadth):
+                    cost = int(self.data[ny][nx])
+                    if (dx == drx) and (dy == dry):
+                        if dist < 3:
+                            heappush(self.queue,(heat + cost,nx,ny,dx,dy,dist + 1))
+                        continue
+                    heappush(self.queue,(heat + cost,nx,ny,dx,dy,1))
+
+                    
+    
+            
+        print("Done! -- something went wrong :(")
+    
+    def display(self,h):
+        temp = [["." for j in range(self.length)] for i in range(self.breadth)]
+        for i in self.seen:
+            temp[i[1]][i[0]] = "#"
+        for i in temp:
+            print("".join(i))
+        print(h)
+        a = input()
+    
+    def sorter(self,x):
+        return x[-1]  
+
+
+
+
 def parser(data):
     return data.split("\n")
 
-def main(data):
-    data = parser(data)
-    path = Path(data)
-
-
-class Path:
-    def __init__(self,data):
-        self.data = data
-        self.length, self.breadth = len(self.data[0]),len(self.data)
-        self.inversedir = {"n":"s","s":"n","e":"w","w":"e"}
-        self.movedirs = {"n":(0,-1),"s":(0,1),"e":(1,0),"w":(-1,0)}
-        self.heatloss = float('inf')
-
-        self.startmove(0,0)
-    def startmove(self,x,y):
-        self.Stack = []
-        self.Stack.append((x,y,'e',1,100))
-        self.Stack.append((x,y,'s',1,100))
-        while self.Stack != []:
-            
-        
-            
-            x,y,dirs,dist,heat = self.Stack.pop()
-            self.move(x,y,dirs,dist,heat)
-            self.display(x,y)
-            print(x,y,dirs,dist,heat)
-            a = input()
-        print(self.heatloss)
-
-        
-    
-
-    def move(self,x,y,direction,distance,heat):
-        
-        if (x,y) == (self.length - 1,self.breadth - 1):
-            self.heatloss = min(self.heatloss,heat)
-            return
-        if heat > self.heatloss:
-            return 
-        dirs = ['n','s','e','w']
-        
-        dirs.remove(self.inversedir[direction])
-        
-        if distance == 3:
-            dirs.remove(direction)
-        newdirs = []
-        for i in dirs:
-            nx,ny = x + self.movedirs[i][0],y + self.movedirs[i][1]
-            if( nx  in range(self.length)) and (ny in range(self.breadth)):
-                newdirs.append((nx,ny,i))
-        
-        print(dirs,sorted(newdirs,key = self.power))
-
-                
-
-        dirs = sorted(newdirs,key = self.power)
-        
-       
-        
-        for i in dirs:
-            if i[2] == direction:
-                self.Stack.append((nx,ny,i[2],distance + 1,heat + int(self.data[y][x])))
-            else:
-                self.Stack.append((nx,ny,i[2],1,heat + int(self.data[y][x])))
-    
-    
-    def display(self,x,y):
-        for j in range(self.breadth):
-            for i in range(self.length):
-                if (i,j) == (x,y):
-                    print("*",end = "")
-                else:
-                    print(self.data[j][i],end = "")
-            print()
-        print()
-    def power(self,a):
-        return abs(self.length - a[0]) + abs(self.breadth - a[1])
-         
-
-
-
-        
-
-
-        
-
-
-
-    
-
-
-with open("day17/data.txt") as file:
-    input_data = file.read()
-    main(test)
-
+with open("day17/data.txt") as f:
+    inpdata = f.read()
+    a = parser(inpdata)
+    ss = Heatsolver(a)
--- day17/part2.py
+++ day17/part2.py
@@ -1,8 +1,104 @@
+from heapq import heappush,heappop
+test = """2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533"""
+
+test2 = """111111111111
+999999999991
+999999999991
+999999999991
+999999999991"""
 
 
-with open("day17/part1.py") as file:
-    data = file.read()
 
-with open("day17/part2.py","w") as file:
-    file.write(data)
 
+
+
+class Heatsolver:
+    def __init__(self,data):
+        self.data = data
+        self.length = len(self.data[0])
+        self.breadth = len(self.data)
+
+        self.solve()
+    
+    def solve(self):
+        #              x,y,drx,dry dist heat
+        self.queue = [(0,0,0,0,0,0)]
+        self.seen = set()
+
+        while self.queue:
+            # self.queue.sort()
+            
+            heat,x,y,drx,dry,dist = heappop(self.queue)
+            
+            # self.display(heat)
+
+            if (x == (self.length - 1)) and (y == (self.breadth - 1)) and (dist > 4):
+                print("Heat is :")
+                print(heat)
+                return
+                
+
+            if (x,y,drx,dry,dist) in self.seen:
+                continue
+            self.seen.add((x,y,drx,dry,dist))
+
+            if dist < 10 and (drx,dry) != (0,0):
+                nx, ny = x + drx, y + dry
+                if (nx in range(self.length)) and (ny in range(self.breadth)):
+                    cost = int(self.data[ny][nx])
+                    heappush(self.queue, (heat + cost, nx, ny, drx, dry, dist + 1))
+                    
+            if dist >= 4 or (drx, dry) == (0, 0):
+                for dx, dy in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+                    if (dx, dy) != (drx, dry) and (dx, dy) != (-drx, -dry):
+                    
+                        nx, ny = x + dx, y + dy
+                        if (nx in range(self.length)) and (ny in range(self.breadth)):
+                            cost = int(self.data[ny][nx])
+                            
+                            heappush(self.queue, (heat + cost, nx, ny, dx, dy, 1))
+            
+         
+    
+
+
+
+
+                    
+    
+            
+        print("Done! -- something went wrong :(")
+    
+    def display(self,h):
+        temp = [["." for j in range(self.length)] for i in range(self.breadth)]
+        for i in self.seen:
+            temp[i[1]][i[0]] = "#"
+        for i in temp:
+            print("".join(i))
+        print(h)
+        a = input()
+    
+
+
+
+
+
+def parser(data):
+    return data.split("\n")
+
+with open("day17/data.txt") as f:
+    inpdata = f.read()
+    a = parser(inpdata)
+    ss = Heatsolver(a)
